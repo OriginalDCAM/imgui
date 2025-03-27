fee4fc0713341801c00f4c4054be546d907d8e28
TrueStatement <denzelannette@hotmail.com>
2025-03-27 17:47:46 +0100

    Added a simple build file

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "ImGui"
+  kind "StaticLib"
+  language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objectdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+      "imconfig.h",
+      "imgui.h",
+      "imgui.cpp",
+      "imgui_draw.cpp",
+      "imgui_internal.h",
+      "imgui_tables.cpp",
+      "imgui_widgets.cpp",
+      "imgui_demo.cpp",
+      "imstb_rectpack.h",
+      "imstb_textedit.h",
+      "imstb_truetype.h"
+    }
+
+    filter "system:windows"
+    systemversion "latest"
+    cppdialect "C++20"
+    staticruntime "On"
+
+    filter { "system:windows", "configuration:Release" }
+      buildoptions "/MT"
